8474f2c6ba481465c9de9ea5eff0d3a490644edb
xineil <xineil@163.com>
2014-07-06 23:23:57 +0800

    打扫代码

--- src/main/java/org/lexuewang/gateway/rest/RestException.java
+++ src/main/java/org/lexuewang/gateway/rest/RestException.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
  */
 public class RestException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
 	public HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
 	public RestException() {
